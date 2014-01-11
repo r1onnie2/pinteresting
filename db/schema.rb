ffe3de46ffa0311a04e38afdf6c61211d17ddb3d
Ronnie Thomas Jr <r1onnie2@yahoo.com>
2014-01-11 12:22:23 -0600

    Updated db migrate file for add use id pins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140109022758) do
+ActiveRecord::Schema.define(version: 20140111181407) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
